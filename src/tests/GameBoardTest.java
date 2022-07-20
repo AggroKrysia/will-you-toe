be75ed38da013a93df2d7c3ba3fd35690eec31c1
Krysia <krysia.durnotki@protonmail.com>
2022-07-20 19:41:21 +0200

    Before adding tests v2

---
+++ src/tests/GameBoardTest.java
@@ -0,0 +1,15 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class GameBoardTest extends TestCase {
+
+    @Test
+    public void putPiece() {
+    }
+
+    @Test
+    public void checkForWin() {
+    }
+}
