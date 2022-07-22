3fa737638f2ec8ca1c2bac9d9caaf9d4fab00ddb
Krysia <krysia.durnotki@protonmail.com>
2022-07-22 18:28:04 +0200

    Add check for win plus tests, first attempt for TDD

--- src/little/code/GameBoard.java
+++ src/little/code/GameBoard.java
@@ -1,16 +1,16 @@
 package little.code;
 
 public class GameBoard {
-    public Pieces[][] board;
-    int WIDTH = 3;
-    int HEIGHT = 3;
+    private Pieces[][] board;
+    private final static int WIDTH = 3;
+    private final static int HEIGHT = 3;
 
     public GameBoard() {
         board = new Pieces[WIDTH][HEIGHT];
         emptyBoard();
     }
 
-    private void emptyBoard() {
+    public void emptyBoard() {
         for (int i = 0; i < WIDTH; i++) {
             for (int j = 0; j < HEIGHT; j++) {
                 board[i][j] = Pieces.EMPTY;
@@ -22,22 +22,77 @@ public class GameBoard {
         board[x][y] = piece;
     }
 
-    public void checkForWin() {
-        Pieces checkedPiece = board[0][0];
-        int i = 0;
-        int j = 0;
-        while (i < HEIGHT) {
-            if (!checkedPiece.equals(board[i][i])) {
-                System.out.println("Nie bangla");
-                return;
-            }
-            System.out.println("Bangla");
-            i++;
-        }
-//        i = 0;
-//        while (i < HEIGHT) {
-//
-//        }
+    public Pieces getPiece(int x, int y) {
+        return board[x][y];
     }
 
+    public boolean checkForWin() {
+        return checkDianogal() || checkSecondDianogal() || checkVertical() || checkHorizontal();
+    }
+
+    private boolean checkDianogal() {
+        Pieces checkedPiece = board[0][0];
+        int i = 0;
+        while (i < HEIGHT) {
+            if (!checkedPiece.equals(board[i][i])) {
+                return false;
+            }
+            i++;
+        }
+        return true;
+    }
+
+    private boolean checkSecondDianogal() {
+        Pieces checkedPiece = board[0][HEIGHT - 1];
+        int i = HEIGHT - 1;
+        int j = 0;
+        while (i >= 0) {
+            if (!checkedPiece.equals(board[j][i])) {
+                return false;
+            }
+            j++;
+            i--;
+        }
+        return true;
+    }
+
+    private boolean checkVertical() {
+        Pieces checkedPiece = board[0][0];
+        for (int i = 0; i < HEIGHT; i++) {
+            if (checkRow(checkedPiece, i)) {
+                return true;
+            }
+            checkedPiece = board[0][i];
+        }
+        return false;
+    }
+
+    private boolean checkRow(Pieces checkedPiece, int y) {
+        for(int i = 0; i < WIDTH; i++){
+            if (!checkedPiece.equals(board[i][y])) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    private boolean checkHorizontal(){
+        Pieces checkedPiece = board[0][0];
+        for (int i = 0; i < WIDTH; i++) {
+            if (checkColumn(checkedPiece, i)) {
+                return true;
+            }
+            checkedPiece = board[i][0];
+        }
+        return false;
+    }
+
+    private boolean checkColumn(Pieces checkedPiece, int x){
+        for(int i = 0; i < HEIGHT; i++){
+            if (!checkedPiece.equals(board[x][0])) {
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- src/tests/GameBoardTest.java
+++ src/tests/GameBoardTest.java
@@ -1,19 +1,66 @@
-import junit.framework.TestCase;
 import little.code.GameBoard;
 import little.code.Pieces;
+import org.junit.Assert;
 import org.junit.Test;
 
-public class GameBoardTest extends TestCase {
+public class GameBoardTest {
 
     GameBoard testBoard = new GameBoard();
+
     @Test
-    public void testputPieceTest() {
-        testBoard.putPiece(0,0, Pieces.CIRCLE);
-        assertEquals(Pieces.CIRCLE.toString(), testBoard.board[0][0].toString());
+    public void testPutPiece() {
+        testBoard.emptyBoard();
+        testBoard.putPiece(0, 0, Pieces.CIRCLE);
+        Assert.assertEquals(Pieces.CIRCLE.toString(), testBoard.getPiece(0,0).toString());
     }
 
     @Test
-    public void testcheckFaorWin() {
-        assertTrue(1<2);
+    public void testCheckForWinDiagonal() {
+        testBoard.emptyBoard();
+        testBoard.putPiece(0, 0, Pieces.CROSS);
+        testBoard.putPiece(1, 1, Pieces.CROSS);
+        testBoard.putPiece(2, 2, Pieces.CROSS);
+
+        Assert.assertTrue(testBoard.checkForWin());
     }
+    @Test
+    public void testCheckForWinSecondDiagonal() {
+        testBoard.emptyBoard();
+        testBoard.putPiece(0, 2, Pieces.CROSS);
+        testBoard.putPiece(1, 1, Pieces.CROSS);
+        testBoard.putPiece(2, 0, Pieces.CROSS);
+
+        Assert.assertTrue(testBoard.checkForWin());
+    }
+
+    @Test
+    public void testCheckForWinVertical() {
+        testBoard.emptyBoard();
+        testBoard.putPiece(0, 0, Pieces.CROSS);
+        testBoard.putPiece(1, 0, Pieces.CROSS);
+        testBoard.putPiece(2, 0, Pieces.CROSS);
+
+        Assert.assertTrue(testBoard.checkForWin());
+    }
+
+    @Test
+    public void testCheckForWinHorizontal() {
+        testBoard.emptyBoard();
+        testBoard.putPiece(0, 0, Pieces.CROSS);
+        testBoard.putPiece(0, 1, Pieces.CROSS);
+        testBoard.putPiece(0, 2, Pieces.CROSS);
+
+        Assert.assertTrue(testBoard.checkForWin());
+    }
+
+    @Test
+    public void testCheckForWinHorizontalOnSecondRow() {
+        testBoard.emptyBoard();
+        testBoard.putPiece(1, 0, Pieces.CROSS);
+        testBoard.putPiece(1, 1, Pieces.CROSS);
+        testBoard.putPiece(1, 2, Pieces.CROSS);
+
+        Assert.assertTrue(testBoard.checkForWin());
+    }
+
 }
